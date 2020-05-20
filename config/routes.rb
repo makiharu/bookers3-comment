eabc25e41bb1941d0666e164aa79e303caee7d35
makiharu <kitakazetoshirokuma@gmail.com>
2020-05-20 10:49:56 +0000

    modiefied

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,11 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books do
-    resources :book_comments, only: [:create]
+  
+  resources :books, only: [:index, :show, :edit, :create, :update, :destroy] do
+    resource :book_comments, only: [:create, :destroy]
     resource :favorites, only: [:create, :destroy]
   end
-
-  resources :book_comments, only: [:destroy]　#createのようにネストさせる必要はなし
   
  #resource :book_comments, only: [:create, :destroy]
 end
